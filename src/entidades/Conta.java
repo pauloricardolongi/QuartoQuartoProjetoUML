0b2a50b0b5d25a209abd75add9ebc977c3e00d50
pauloricardolongi <pauloricardolongi@gmail.com>
2021-12-02 03:30:50 -0300

    Classe Conta

---
+++ src/entidades/Conta.java
@@ -0,0 +1,47 @@
+package entidades;
+
+public class Conta {
+	
+	private Integer numero;
+	private String titular;
+	private Double saldo;
+	
+	public Conta() {
+		
+	}
+
+	public Conta(Integer numero, String titular, Double saldo) {
+		
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+
+	public void saque(double montante) {
+		saldo = saldo - montante;
+	}
+	public void deposito(double montante) {
+		saldo = saldo + montante;
+	}
+
+}
